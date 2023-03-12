efba6c22f573751527d1b1133783d198f6034d3a
Lennard Dorst <lennardsolana@gmail.com>
2023-03-12 13:10:03 +0100

    Adding Capacitor, etc.

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.weekend.build',
+  appName: 'weekend-build-template',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
