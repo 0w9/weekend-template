25234d6663e81108cc3d403d4889b0ce55a142d2
Lennard Dorst <lennardsolana@gmail.com>
2023-03-12 13:33:46 +0100

    Adding Capacitor.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.weekend.build',
   appName: 'weekend-build-template',
-  webDir: 'public',
+  webDir: 'out',
   bundledWebRuntime: false
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,15 @@ import '../styles/globals.css'
 import { useEffect } from 'react'
 import posthog from 'posthog-js'
 
-useEffect(() => {
-  posthog.init(
-    process.env.POSTHOG_API_KEY,
-    {
-      api_host: process.env.POSTHOG_API_HOST
-    })
-}, [])
-
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    posthog.init(
+      process.env.POSTHOG_API_KEY,
+      {
+        api_host: process.env.POSTHOG_API_HOST
+      })
+  }, [])
+  
   return <Component {...pageProps} />
 }
